cc6382d28580696710b00fc152063868de9654d0
rezaeim <meh.rezaei08@gmail.com>
2024-12-10 15:04:18 -0800

    minor

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,14 +1,16 @@
 # Import all the required libraries
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
 from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
+
+from src.exception import CustomException
+from src.logger import logging
 
 # Initialize Data Ingestion Configuration
 @dataclass
